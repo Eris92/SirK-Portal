80a7b4b78859774eb56625eb22793ce17bb9bf1f
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-20 17:37:31 +0200

    Use module-local Files directory for My Commands sources

--- modules/commands/index.js
+++ modules/commands/index.js
@@ -1,10 +1,23 @@
 "use strict";
 
+var fs = require("fs");
+var path = require("path");
+
 exports.create = function (runtime) {
+    var moduleRoot = __dirname;
+    var filesRoot = path.join(moduleRoot, "Files");
+    var legacyRoot = path.resolve(moduleRoot, "..", "..", "legacy", "commands");
+    var sourceRoot = fs.existsSync(path.join(filesRoot, "package.json")) ? filesRoot : legacyRoot;
+
     return {
         name: "commands",
+        sourceRoot: sourceRoot,
+        filesRoot: filesRoot,
         initialize: function () {
-            runtime.audit("mycompanymoduleloaded", "Commands module loaded.");
+            if (!fs.existsSync(sourceRoot)) {
+                throw new Error("My Commands source files are missing. Run npm run sync:files.");
+            }
+            runtime.audit("mycompanymoduleloaded", "Commands module loaded from " + sourceRoot + ".");
         }
     };
 };
