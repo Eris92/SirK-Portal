7571a3717dc9e4ec43ea1e46692a7966ac4b0d08
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-21 21:25:48 +0200

    feat(portal): register Portal in MyCompany runtime

---
+++ core/runtime-portal.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var shared = require("./shared.js");
+var baseFactory = require("./runtime.js");
+var portalFactory = require("../modules/Portal/index.js");
+
+var VERSION = "1.4.0";
+var PORTAL_DEFAULTS = {
+    enabled: false,
+    defaultView: "overview",
+    showLauncher: true
+};
+
+module.exports.createRuntime = function (options) {
+    var runtime = baseFactory.createRuntime(options);
+    var context = runtime.context;
+
+    context.settings.defaults.modules = context.settings.defaults.modules || {};
+    context.settings.defaults.modules.portal = shared.copy(PORTAL_DEFAULTS);
+
+    runtime.modules.portal = portalFactory.createModule(context);
+
+    var originalAdminSnapshot = runtime.adminSnapshot;
+    runtime.adminSnapshot = function (user) {
+        var value = originalAdminSnapshot(user);
+        if (value && value.plugin) value.plugin.version = VERSION;
+        return value;
+    };
+
+    runtime.bootstrap = function (user) {
+        var result = {};
+        Object.keys(runtime.modules).forEach(function (key) {
+            var module = runtime.modules[key];
+            result[key] = {
+                enabled: context.settings.isModuleEnabled(key),
+                ready: !module.__loadError,
+                error: module.__loadError || null,
+                config: module.clientConfig(),
+                access: module.getAccess(user)
+            };
+        });
+        return {
+            ok: true,
+            version: VERSION,
+            modules: result
+        };
+    };
+
+    var originalRequest = runtime.request;
+    runtime.request = function (method, moduleName, asset, req, res, user) {
+        if (moduleName === "_runtime" && method === "GET") {
+            shared.sendJson(res, 200, runtime.bootstrap(user));
+            return;
+        }
+        return originalRequest(method, moduleName, asset, req, res, user);
+    };
+
+    runtime.version = VERSION;
+    return runtime;
+};
