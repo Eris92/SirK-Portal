73a84ca38cd776b664ee6714456e5a67b4e6ee27
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-21 21:33:38 +0200

    refactor(portal): use single MyCompany runtime instance

--- plugin-main-1.4.0.js
+++ plugin-main-1.4.0.js
@@ -1,16 +1,3 @@
 "use strict";
 
-var base = require("./plugin-main.js");
-var runtimeFactory = require("./core/runtime-portal.js");
-var adminFactory = require("./MyCompanyAdmin.js");
-
-module.exports.createPlugin = function (parent, shortName) {
-    var plugin = base.createPlugin(parent, shortName);
-    plugin.runtime = runtimeFactory.createRuntime({
-        parent: parent,
-        pluginRoot: __dirname,
-        source: plugin
-    });
-    plugin.admin = adminFactory.admin(plugin);
-    return plugin;
-};
+module.exports = require("./plugin-main.js");
