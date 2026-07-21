ac20c11e39906e40224604a44de7f96c2a75038e
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-21 23:33:14 +0200

    fix(portal): normalize MyScripts folder icon data before render

---
+++ public/portal-icon-data.js
@@ -0,0 +1,62 @@
+(function () {
+    "use strict";
+
+    if (window.__myCompanyPortalIconDataLoaded) return;
+    window.__myCompanyPortalIconDataLoaded = true;
+
+    var core = window.MyCompanyCore;
+    if (!core || typeof core.api !== "function" || typeof core.post !== "function") return;
+
+    function safeIconSource(value) {
+        value = String(value || "").trim();
+        if (!value) return "";
+        if (/^data:image\/(?:svg\+xml|png|gif|jpeg|webp);/i.test(value)) return value;
+        if (/^(?:https?:\/\/|\/|\.\/|\.\.\/|pluginadmin\.ashx)/i.test(value)) return value;
+        return "";
+    }
+
+    function escapeAttribute(value) {
+        return String(value)
+            .replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+    }
+
+    function normalizeNode(node) {
+        if (!node || typeof node !== "object") return node;
+
+        var source = safeIconSource(node.iconData);
+        if (source) {
+            node.icon = '<img class="sirk-management-folder-image" alt="" src="' + escapeAttribute(source) + '">';
+        } else if (/\.svg(?:\?.*)?$/i.test(String(node.icon || "")) || /[\\/]/.test(String(node.icon || ""))) {
+            node.icon = "";
+        }
+
+        (node.children || []).forEach(normalizeNode);
+        return node;
+    }
+
+    function normalizeResponse(value) {
+        if (!value || typeof value !== "object") return value;
+        if (value.tree) normalizeNode(value.tree);
+        if (value.script) normalizeNode(value.script);
+        return value;
+    }
+
+    var originalApi = core.api;
+    core.api = function (moduleName, assetName, options, parameters) {
+        return originalApi.call(core, moduleName, assetName, options, parameters).then(function (response) {
+            if (moduleName === "myscripts") normalizeResponse(response);
+            return response;
+        });
+    };
+
+    var originalPost = core.post;
+    core.post = function (moduleName, assetName, values) {
+        return originalPost.call(core, moduleName, assetName, values).then(function (response) {
+            if (moduleName === "myscripts") normalizeResponse(response);
+            return response;
+        });
+    };
+}());
