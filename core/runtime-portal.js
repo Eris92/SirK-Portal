682189f4529cc50590c5e94274a2ede0344996a2
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-22 09:24:57 +0200

    chore: synchronize portal runtime version 1.5.8

--- core/runtime-portal.js
+++ core/runtime-portal.js
@@ -4,7 +4,7 @@ var shared = require("./shared.js");
 var baseFactory = require("./runtime.js");
 var portalFactory = require("../modules/Portal/index-safe.js");
 
-var VERSION = "1.5.4";
+var VERSION = "1.5.8";
 var PORTAL_DEFAULTS = {
     enabled: false,
     defaultView: "overview",
