ecc60471234b42f1f7649e69141b1da04e9e2efb
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-22 22:36:36 +0200

    test: validate access save and Desktop connection controls

---
+++ test/portal-access-and-desktop-connect.test.js
@@ -0,0 +1,18 @@
+"use strict";
+var assert = require("assert");
+var fs = require("fs");
+var path = require("path");
+var root = path.resolve(__dirname, "..");
+function read(file) { return fs.readFileSync(path.join(root, file), "utf8"); }
+var admin = read("views/MyCompany.handlebars");
+var portal = read("public/portal-standalone.html");
+assert(admin.indexOf("Dostęp dla wszystkich użytkowników") >= 0);
+assert(admin.indexOf('dispatchEvent(new Event("change"') >= 0);
+assert(portal.indexOf('select.value="3"') >= 0);
+assert(portal.indexOf('select.value="2"') >= 0);
+assert(portal.indexOf("Zapytaj o zgodę + Bar") >= 0);
+assert(portal.indexOf("Zapytaj o zgodę") >= 0);
+assert(portal.indexOf("Pasek Prywatności") >= 0);
+assert(portal.indexOf("Number(amt.state)===2") >= 0);
+assert(portal.indexOf("pendingConsent") >= 0);
+console.log("Portal allowAll save and Desktop connection controls: OK");
