3fe617052ad622d958df28399b4d929cd08dcd57
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-22 08:26:42 +0200

    chore: invalidate MyCompany 1.5.4 assets

--- public/core.js
+++ public/core.js
@@ -2,7 +2,7 @@
     "use strict";
     window.MyCompanyCore = window.MyCompanyCore || {};
     var core = window.MyCompanyCore;
-    core.assetVersion = "1.5.3";
+    core.assetVersion = "1.5.4";
 
     function svgData(svg) {
         return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svg);
