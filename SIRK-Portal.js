73c4e87009a7426c8b5b281f5b07ac01be3d8cac
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-24 07:29:17 +0200

    fix: remove unsafe hyphenated MeshCentral entrypoint

--- SIRK-Portal.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var implementation = require("./plugin-main-standalone.js");
-
-module.exports["SIRK-Portal"] = function (parent) {
-    return implementation.createPlugin(parent, "SIRK-Portal");
-};
