10abc79f253e57515c1e4230fbedee48dd0561fa
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-24 15:18:49 +0200

    feat: register independent updater before MeshCentral Portal routes

--- SIRKPortal.js
+++ SIRKPortal.js
@@ -2,12 +2,14 @@
 
 var implementation = require("./plugin-main-standalone.js");
 var routeCompat = require("./server/core/express-route-compat.js");
+var updateBridge = require("./server/core/plugin-update-bridge.js");
 
 module.exports.SIRKPortal = function (parent) {
     var plugin = implementation.createPlugin(parent, "SIRKPortal");
     var setupHttpHandlers = plugin.hook_setupHttpHandlers;
 
     plugin.hook_setupHttpHandlers = function (webserver, meshServer) {
+        updateBridge.install(plugin, parent, webserver, meshServer);
         return routeCompat.withExactPortalRedirect(webserver && webserver.app, function () {
             return setupHttpHandlers.call(plugin, webserver, meshServer);
         });
